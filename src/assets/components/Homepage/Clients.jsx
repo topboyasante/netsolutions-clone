43f0e1f2491a4dc2880156117843e84c1fdbfa22
Nana K <73991180+topboyasante@users.noreply.github.com>
2023-01-18 15:16:59 +0000

    Progress.

--- src/assets/components/Homepage/Clients.jsx
+++ src/assets/components/Homepage/Clients.jsx
@@ -3,8 +3,15 @@ import React from 'react'
 function Clients() {
   return (
     <main className='w-screen h-screen'>
-        <section cl>
-
+        <section className='max-w-[1600px] mx-auto h-full'>
+            <h1 className="text-6xl font-extrabold">Clients</h1>
+            <br />
+            <p className='my-5 text-3xl'>Whether as a website development agency or app development agency, 
+                Net Solutions has played an integral part in shaping the web presence of some elite brands. 
+                We’ve had the pleasure of growing brands in:
+            </p>
+            <br />
+            <img src="https://www.netsolutions.com/assets/images/home-client.webp" alt="clients" className='mx-auto'/>
         </section>
     </main>
   )
